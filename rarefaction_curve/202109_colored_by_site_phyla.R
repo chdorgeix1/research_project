8c2b883ddbeb91d5d11f06155b3c610cea7f75a9
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-03-31 14:38:10 -0400

    Created outputs of individual site genera rarefaction curves

--- rarefaction_curve/202109_colored_by_site_phyla.R
+++ rarefaction_curve/202109_colored_by_site_phyla.R
@@ -2,7 +2,7 @@ require(vegan)
 
 setwd("C:/Users/15505/Documents/GitHub/research_project/rarefaction_curve")
 
-df <- read.csv("202109_phylum_with_site.csv", header = TRUE, sep = ",")
+df <- read.csv("Users/15505/Documents/GitHub/research_project/rarefaction_curve/full_202109_genera_with_site.csv", header = TRUE, sep = ",")
 
 unique(df[c('site')])
 
--- rarefaction_curve/individual_site_rarefaction.R
+++ rarefaction_curve/individual_site_rarefaction.R
@@ -2,11 +2,27 @@ require(vegan)
 
 setwd("C:/Users/15404/Documents/GitHub/research_project/rarefaction_curve/")
 
-df <- read.csv("202109_phylum_with_site.csv", header = TRUE, sep = ",")
+df <- read.csv("full_202109_genera_with_site.csv", header = TRUE, sep = ",")
 
-df1 <- subset(df, site == 'KCKT')
+unique(df$site)
 
 #df1 <- df
+df1 <- subset(df, site == 'KCKT')
+
+numsites = (dim(unique(df1[c('site')]))[1])
+print(numsites)
+
+coldf = df[c('site')]
+
+coldf[coldf == "WART"] <- 'darkred'
+#coldf[coldf == "SBR"] <- 'forestgreen'
+#coldf[coldf == "BTU"] <- 'yellow'
+#coldf[coldf == "SKY"] <- 'blue'
+#coldf[coldf == "BLDTTT"] <- 'red'
+#coldf[coldf == "KCKT"] <- 'orange'
+  
+# Convert the color list to a vector
+collist <- as.vector(unlist(coldf))
 
 df2 <- df1
 df2[df2 == 0] <- 25
@@ -24,6 +40,6 @@ S <- specnumber(df1)
 Srare <- rarefy(df1[][5:ncol(df1)], raremax)
 
 # Plot the rarefaction curve with colored lines
-rarecurve(df1[][5:ncol(df1)], step = 20, sample = raremax, col = 'blue', lwd = 2, cex = 0.5)
+rarecurve(df1[][5:ncol(df1)], step = 20, sample = raremax, col = 'orange', lwd = 2, cex = 0.5)
 
           
